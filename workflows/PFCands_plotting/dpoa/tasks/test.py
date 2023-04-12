3b274da43d1716a56a87ed18ccc71780d39b4fee
alextintin007 <alextintin007@outlook.com>
2023-04-12 12:48:01 -0500

    workflows-PFCands: working workflow

--- workflows/PFCands_plotting/dpoa/tasks/test.py
+++ workflows/PFCands_plotting/dpoa/tasks/test.py
@@ -6,48 +6,123 @@ import law
 from dpoa.tasks.base import Task
 
 class NanoProducer(Task):
-    sandbox = "docker::rootproject/root:latest"
+	sandbox = "docker::riga/py-sci"
+	
+	def output(self):
+		return self.local_target("some_fake_file.txt")
+	
+	def run(self):
+		output = self.output()
+		output.parent.touch()
+		
+		cmd = f"echo 'Hello!' > {output.basename};echo 'World!' >> {output.basename}"
+		p, _, _ = law.util.interruptable_popen(
+			cmd,
+			shell=True,
+			executable="/bin/bash",
+			cwd=output.parent.path,
+		)
+		
+		if p != 0:
+			raise Exception("command failed")
+        
+class Repository(Task):
+    sandbox = "docker::riga/py-sci"
 
     def output(self):
-        return self.local_target("some_fake_file.txt")
+        return self.local_target("cat-hackathon")
 
     def run(self):
         output = self.output()
         output.parent.touch()
 
-        cmd = f"echo 'Hello, world!' >> {output.basename}"
+        # Download the python file using wget
+        github = "https://github.com/cms-dpoa/cat-hackathon.git"
+        cmd = f"git clone {github};"\
+            "ls;"
         p, _, _ = law.util.interruptable_popen(
             cmd,
             shell=True,
             executable="/bin/bash",
-            cwd=output.parent.path,
+            cwd=output.parent.path
         )
         if p != 0:
             raise Exception("command failed")
 
 
-class CreatePlots(Task):
-    sandbox = "docker::riga/py-sci"
+class CoffeaPlotting(Task):
+    sandbox = "docker::coffeateam/coffea-base:latest"
 
     def requires(self):
-        return NanoProducer.req(self)
+        return Repository.req(self)
 
     def output(self):
-        return self.local_target("some_nice_plot.png")
+        return self.local_target("coffea_output")
 
-    @law.decorator.safe_output
     def run(self):
-        # create the output directory
         output = self.output()
         output.parent.touch()
 
-        fake_img_url = "https://raw.githubusercontent.com/garrettj403/SciencePlots/master/examples/figures/fig1.jpg"  # noqa
-        cmd = f"wget -q -O {output.basename} {fake_img_url}"
+        # Download the python file using wget
+        github = "https://github.com/cms-dpoa/cat-hackathon.git"
+        cmd = f"mkdir coffea_output;"\
+            "cd ../Repository/cat-hackathon/analysis/coffea/;"\
+            "python coffea_plot.py;"\
+            "mv *.png ../../../../CoffeaPlotting/coffea_output;"\
+            "mv PF_n.txt ../../../../CoffeaPlotting/coffea_output;"
         p, _, _ = law.util.interruptable_popen(
             cmd,
             shell=True,
             executable="/bin/bash",
-            cwd=output.parent.path,
+            cwd=output.parent.path
         )
         if p != 0:
-            raise Exception("fake image download failed")
+            raise Exception("command failed")
+
+class RDFPlotting(Task):
+    sandbox = "docker::rootproject/root:latest"
+
+    def requires(self):
+        return Repository.req(self)
+
+    def output(self):
+        return self.local_target("rdataframe_output")
+
+    def run(self):
+        output = self.output()
+        output.parent.touch()
+
+        # Download the python file using wget
+        github = "https://github.com/cms-dpoa/cat-hackathon.git"
+        cmd = f"mkdir rdataframe_output;"\
+            "cd ../Repository/cat-hackathon/analysis/rdataframe/;"\
+            "python rdf_plot.py;"\
+            "mv *.png ../../../../RDFPlotting/rdataframe_output;"
+        p, _, _ = law.util.interruptable_popen(
+            cmd,
+            shell=True,
+            executable="/bin/bash",
+            cwd=output.parent.path
+        )
+        if p != 0:
+            raise Exception("command failed")
+
+class Final(Task):
+    sandbox = "docker::riga/py-sci"
+
+    def requires(self):
+        return [RDFPlotting.req(self), CoffeaPlotting.req(self)]
+    def output(self):
+        return self.local_target("some_fake_file.txt")
+    def run(self):
+        output = self.output()
+        output.parent.touch()
+        cmd = f"echo 'Hello!' > {output.basename};echo 'World!' >> {output.basename}"
+        p, _, _ = law.util.interruptable_popen(
+			cmd,
+			shell=True,
+			executable="/bin/bash",
+			cwd=output.parent.path,
+		)
+        if p != 0:
+            raise Exception("command failed")
