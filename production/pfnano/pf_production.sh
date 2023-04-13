989f12101f8df7142b6c579d0905f9aad71827bd
alextintin007 <alextintin007@outlook.com>
2023-04-13 13:20:29 -0500

    github-workflows: test

--- production/pfnano/pf_production.sh
+++ production/pfnano/pf_production.sh
@@ -12,7 +12,9 @@ set -e
 # then
    sudo chown $USER /mnt/vol
    echo mount volume
-   ls -l /mnt/vol
+   ls /mnt/vol
+   cp /mnt/vol/data/doubleeg_nanoaod_eg.root output1.root
+   ls
    #echo At the start SITECONFIG_PATH: $SITECONFIG_PATH
    #export SITECONFIG_PATH=/mnt/vol/production/pfnano
 #   cp -r /mnt/vol cat-hackathon
@@ -30,11 +32,11 @@ set -e
 # git cms-merge-topic 39040
 # git clone -b opendata https://github.com/DAZSLE/PFNano.git PhysicsTools/PFNano
 # scram b -j 4
+
 echo pwd
 pwd
+cp /data/doubleeg_nanoaod_eg.root output2.root
 ls
-
-# cp /mnt/vol/data/doubleeg_nanoaod_eg.root output.root
 
 # cmsDriver.py --python_filename doubleeg_cfg.py --eventcontent NANOAOD --datatier NANOAOD \
 #   --fileout file:doubleeg_nanoaod.root --conditions 106X_dataRun2_v36 --step NANO \
